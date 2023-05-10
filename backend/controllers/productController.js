fcb0b37d7a991bf792c0af53fe6ca216e08e98bc
Anthony Dsouza <anthonydsouza1728@gmail.com>
2023-05-10 17:53:50 +0530

    Final Completion

---
+++ backend/controllers/productController.js
@@ -0,0 +1,146 @@
+import asyncHandler from "express-async-handler";
+
+import Product from "../models/productModel.js";
+
+/**
+ * @desc 		Get all products
+ * @route		GET /api/products
+ * @access	public
+ */
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+/**
+ * @desc 		Get single products
+ * @route		GET /api/products/:id
+ * @access	public
+ */
+const getProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+/**
+ * @desc 		Delete a product
+ * @route		DELETE /api/products/:id
+ * @access	private/admin
+ */
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await Product.deleteOne(product);
+    res.json({ message: "Product deleted" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+/**
+ * @desc 		Create a product
+ * @route		POST /api/products
+ * @access	private/admin
+ */
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Product",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+/**
+ * @desc 		Update a product
+ * @route		PUT /api/products/:id
+ * @access	private/admin
+ */
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+/**
+ * @desc 		Create new review
+ * @route		POST /api/products/:id/reviews
+ * @access	private
+ */
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: +rating,
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReview = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, currVal) => currVal.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+};
---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+import brcypt from "bcryptjs";
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await brcypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await brcypt.genSalt(10);
+  this.password = await brcypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
